e2d9f64d9f268dfbf3f02dd54113d6b546f47584
autocuan <62184941+autocuan@users.noreply.github.com>
2020-07-05 05:12:25 +0700

    Update bareng.php

--- bareng.php
+++ bareng.php
@@ -49,57 +49,6 @@ goto gofood;
 echo "\n".color("red","-] Message: ".$message);
 }
 gofood:
-sleep(7);
-}
-$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2206"}');
-$message = fetch_value($code1,'"message":"','"');
-if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-echo "\n".color("green","🔓▶️ Message: ".$message);
-goto gocar;
-}else{
-echo "\n".color("red","🔐▶️ Message: ".$message);
-gocar:
-echo "\n".color("nevy","=> BISMILLAH");
-echo "\n".color("yellow","=>BISMILLAH ");
-for($a=1;$a<=3;$a++){
-echo color("yellow",".");
-sleep(8);
-}
-$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2206"}');
-$message = fetch_value($code1,'"message":"','"');
-if(strpos($code1, 'Promo kamu sudah bisa dipakai.')){
-echo "\n".color("green","🔓▶️ Message: ".$message);
-goto gofood;
-}else{
-echo "\n".color("red","🔐▶️ Message: ".$message);
-gofood:
-echo "\n".color("nevy","=>BISMILLAH");
-echo "\n".color("yellow","BISMILLAH");
-for($a=1;$a<=3;$a++){
-echo color("yellow",".");
-sleep(9);
-}
-$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PAKEGOFOOD0906"}');
-$message = fetch_value($code1,'"message":"','"');
-echo "\n".color("green","🔓▶️ Message: ".$message);
-echo "\n".color("nevy","BISMILLAH");
-echo "\n".color("yellow","BISMILLAH");
-for($a=1;$a<=3;$a++){
-echo color("yellow",".");
-sleep(10);
-}
-$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2206"}');
-$message = fetch_value($code1,'"message":"','"');
-echo "\n".color("white"," Message: ".$message);
-echo "\n".color("white"," BISMILLAH");
-echo "\n".color("white"," BISMILLAH");
-for($a=1;$a<=3;$a++){
-echo color("white",".");
-sleep(11);
-}
-$boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EATLAH"}');
-$messageboba09 = fetch_value($boba09,'"message":"','"');
-echo "\n".color("green","# Message: ".$messageboba09);
 sleep(3);
 $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=11&page=1', $token);
 $total = fetch_value($cekvoucher,'"total_vouchers":',',');
